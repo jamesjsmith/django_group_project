c3b68575c56a2f4b91c17d5c2ca7d0ec672c90e9
James Smith <user@Users-MacBook-Pro-10.local>
2024-04-24 16:57:50 -0500

    creating the skeleton framework for the project, have to replace "catalog" in the search bar with "weather_app" still trying to figure out how to fix that

--- weather_project/weather_project/urls.py
+++ weather_project/weather_project/urls.py
@@ -16,7 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weather_App/', include('weather_App.urls')),
 ]
+
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url = 'weather_App/', permanent=True))
+]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
